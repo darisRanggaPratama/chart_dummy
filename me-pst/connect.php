05ca97c15f797688ea68d6d1abed6330acd34749
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2023-07-10 20:53:59 +0700

    initial commit

--- me-pst/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Connection details
-$server = 'localhost'; // Server name
-$user = 'hyvoycom_rangga'; // User name
-$password = '20061988rangga'; // User password
-$database = 'hyvoycom_payroll'; // Database name
-
-// Connection to database
-$connect = mysqli_connect($server, $user, $password, $database);
