413056de1af5766a1e02d066a25028ebe9fc0974
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2023-07-10 21:04:46 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# chart_dummy
-chart payroll dummy
+# Web Grafik Gaji dan Tabel (CRUD)
+
+Aplikasi web ini dibuat menggunakan:
+1. PHP 8
+2. MySQL 8
+3. Bootstrap 5
+4. Chart.js
+5. Datatables.js
+
+
---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+// Connection details
+$server = 'localhost'; // Server name
+$user = 'hyvoycom_rangga'; // User name
+$password = '20061988rangga'; // User password
+$database = 'hyvoycom_payroll'; // Database name
+
+// Connection to database
+$connect = mysqli_connect($server, $user, $password, $database);
